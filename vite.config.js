a7a5d2b6ebefc82540266d509911495492cc4b49
unknown <ferlandol@hotmail.com>
2024-12-07 20:13:22 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { remix } from '@remix-run/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [remix()],
     base: './'
 })
